dd745a3b4621c29a3356be28ce970adea3c95a58
YaroslavBiG <yaroslavbig@ya.ru>
2020-06-19 12:10:12 +0200

    feet: add try catch

--- src/js/api/getWords.js
+++ src/js/api/getWords.js
@@ -15,9 +15,13 @@
 async function getWords(page = 0, group = 0, lte = 10, perPage = 20) {
   const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${group}
   &wordsPerExampleSentenceLTE=${lte}&wordsPerPage=${perPage}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    return Object.keys(err);
+  }
 }
 
 export default getWords;
