216eaf375e2da8dd522d1273c1178095ea467ce0
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-06-20 21:06:49 +0400

    chore: delete unused code

--- src/js/utils/load.js
+++ src/js/utils/load.js
@@ -1,12 +1,8 @@
 import { loadAuth } from './loadAuth';
 import { loadUser } from './loadUser';
 import { globalUser } from './main';
-import { createSignInUpPage } from '../pages';
-import { controlButton } from '../form';
 
 export const load = () => {
   loadAuth();
   globalUser.setFullUser(loadUser());
-  document.querySelector('body').innerHTML = createSignInUpPage();
-  controlButton();
 };
