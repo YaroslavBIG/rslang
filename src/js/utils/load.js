fffc75e86802fb68a17e6b6caff464fc6901c33c
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-06-20 21:05:27 +0400

    chore: change color for title icon block

--- src/js/utils/load.js
+++ src/js/utils/load.js
@@ -1,8 +1,12 @@
 import { loadAuth } from './loadAuth';
 import { loadUser } from './loadUser';
 import { globalUser } from './main';
+import { createSignInUpPage } from '../pages';
+import { controlButton } from '../form';
 
 export const load = () => {
   loadAuth();
   globalUser.setFullUser(loadUser());
+  document.querySelector('body').innerHTML = createSignInUpPage();
+  controlButton();
 };
