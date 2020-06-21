0d241f1d42151d49c1499c05209c57bc3eeb7476
Yulia Kravtsova <KostyaEK2006@gmail.com>
2020-06-21 14:20:32 +0300

    fix: fix and update code

---
+++ src/js/clickButtons/clickButtons.js
@@ -0,0 +1,27 @@
+export const clickButtonSpeakIt = () => {
+  document.getElementById('speakItGame').addEventListener('click', () => {});
+};
+
+export const clickButtonEnglishPuzzle = () => {
+  document
+    .getElementById('englishPuzzleGame')
+    .addEventListener('click', () => {});
+};
+
+export const clickButtonSavannahGame = () => {
+  document.getElementById('savannahGame').addEventListener('click', () => {});
+};
+
+export const clickButtonListeningGame = () => {
+  document.getElementById('listeningGame').addEventListener('click', () => {});
+};
+
+export const clickButtonSprintGame = () => {
+  document.getElementById('sprintGame').addEventListener('click', () => {});
+};
+
+export const clickButtonConstructorGame = () => {
+  document
+    .getElementById('constructorGame')
+    .addEventListener('click', () => {});
+};
---
+++ src/js/clickButtons/index.js
@@ -0,0 +1,14 @@
+export { speakitGame } from './promo-pages/speakitGame.js';
+export { englishPuzzleGame } from './promo-pages/englishPuzzleGame.js';
+export { savannahGame } from './promo-pages/savannahGame.js';
+export { listeningGame } from './promo-pages/listeningGame.js';
+export { sprintGame } from './promo-pages/sprintGame.js';
+export { constructorGame } from './promo-pages/constructorGame.js';
+export {
+  clickButtonSpeakit,
+  clickButtonEnglishPuzzle,
+  clickButtonSavannahGame,
+  clickButtonListeningGame,
+  clickButtonSprintGame,
+  clickButtonConstructorGame,
+} from './clickButtons.js';
