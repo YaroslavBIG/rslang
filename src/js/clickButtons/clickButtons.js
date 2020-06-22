a0647b98cbaa75a57148b6b41f2ebd1060c86a04
Yulia Kravtsova <KostyaEK2006@gmail.com>
2020-06-22 13:17:27 +0300

    feat: add exit in promo pages

--- src/js/clickButtons/clickButtons.js
+++ src/js/clickButtons/clickButtons.js
@@ -1,27 +1,35 @@
 export const clickButtonSpeakIt = () => {
-  document.getElementById('speakItGame').addEventListener('click', () => {});
+  document.getElementById('speakItGame').addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'none';
+  });
 };
 
 export const clickButtonEnglishPuzzle = () => {
-  document
-    .getElementById('englishPuzzleGame')
-    .addEventListener('click', () => {});
+  document.getElementById('englishPuzzleGame').addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'none';
+  });
 };
 
 export const clickButtonSavannahGame = () => {
-  document.getElementById('savannahGame').addEventListener('click', () => {});
+  document.getElementById('savannahGame').addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'none';
+  });
 };
 
 export const clickButtonListeningGame = () => {
-  document.getElementById('listeningGame').addEventListener('click', () => {});
+  document.getElementById('listeningGame').addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'none';
+  });
 };
 
 export const clickButtonSprintGame = () => {
-  document.getElementById('sprintGame').addEventListener('click', () => {});
+  document.getElementById('sprintGame').addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'none';
+  });
 };
 
 export const clickButtonConstructorGame = () => {
-  document
-    .getElementById('constructorGame')
-    .addEventListener('click', () => {});
+  document.getElementById('constructorGame').addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'none';
+  });
 };
