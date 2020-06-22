4d63fb67e09ac747f742275309c025e6cbbd012f
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-06-22 13:31:22 +0400

    feat: add style for active button

--- src/js/utils/load.js
+++ src/js/utils/load.js
@@ -1,8 +1,10 @@
 import { loadAuth } from './loadAuth';
 import { loadUser } from './loadUser';
 import { globalUser } from './main';
+import { createSignInUpPage } from '../pages';
 
 export const load = () => {
   loadAuth();
   globalUser.setFullUser(loadUser());
+  document.querySelector('body').innerHTML = createSignInUpPage();
 };
