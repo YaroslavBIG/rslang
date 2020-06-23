2cd0171e4658b1dcd3041fa0ae4bbbcc694f4122
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-06-23 23:06:55 +0400

    fix: delete unnecessary code

--- src/js/utils/load.js
+++ src/js/utils/load.js
@@ -1,10 +1,8 @@
 import { loadAuth } from './loadAuth';
 import { loadUser } from './loadUser';
 import { globalUser } from './main';
-import { createSignInUpPage } from '../pages';
 
 export const load = () => {
   loadAuth();
   globalUser.setFullUser(loadUser());
-  document.querySelector('body').innerHTML = createSignInUpPage();
 };
