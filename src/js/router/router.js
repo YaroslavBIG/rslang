f1d4089a286e72c16cca37eb4013fb6825931a2a
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-06-24 17:54:39 +0400

    chore: delete unnecessary code & return deleted files & chande errors eslint

--- src/js/router/router.js
+++ src/js/router/router.js
@@ -1,73 +1,60 @@
-import { actionAuth } from "../utils";
-import { parseLocation } from "./parseLocation";
-import { ErrorComponent } from "./routerComponents";
-import { controlForm, submitForm } from '../form';
+import { actionAuth } from '../utils';
+import { parseLocation } from './parseLocation';
+import { ErrorComponent } from './routerComponents';
+import { findComponentByPath } from './findComponentByPath';
+import { routes } from './routes';
+import { controlForm, submitForm, chooseIcon } from '../form';
 
 export const router = () => {
   const isAuth = actionAuth.getAuth();
   const path = parseLocation();
 
   const packWithHeader = ['/main/settings', '/main/learning', '/main/method-interval', '/main/english-puzzle', '/main/speak-it',
-  '/main/sprint', '/main/listening', '/main/savannah', '/main/constructor'];
+    '/main/sprint', '/main/listening', '/main/savannah', '/main/constructor'];
 
   if ((isAuth === 'true' || isAuth === true) && path === '/auth') {
-      document.querySelector('.click-enter').click();
+    document.querySelector('.click-enter').click();
   }
 
   const { component = ErrorComponent } = findComponentByPath(path, routes) || {};
 
   if (packWithHeader.includes(path)) {
-    document.querySelector('main').innerHTML =  component.render();
+    document.querySelector('main').innerHTML = component.render();
   } else {
-    document.getElementById('root').innerHTML =  component.render();
+    document.getElementById('root').innerHTML = component.render();
   }
 
-  switch(path) {
-    case '/': {
+  /**
+   * в switch каждый забирает свой кейс, чтобы не пушить пустые болванки.
+   * в нем пишет те функции, которые вызываются у него на странице.
+   * список кейсов:
+   * case '/main': {}
+   * case '/main/settings': {}
+   * case '/main/learning': {}
+   * case '/main/method-interval': {}
+   * case '/main/english-puzzle': {}
+   * case '/main/speak-it': {}
+   * case '/main/sprint': {}
+   * case '/main/listening': {}
+   * case '/main/savannah': {}
+   * case '/main/constructor': {}
+   * case '/vocabulary': {}
+   * case '/statistic': {}
+   * case '/about-us': {}
+   */
 
+  switch (path) {
+    case '/': {
+      break;
     }
     case '/auth': {
       controlForm();
       submitForm();
+      chooseIcon();
+      break;
     }
-    case '/main': {
-
-    }
-    case '/main/settings': {
-
-    }
-    case '/main/learning': {
-
-    }
-    case '/main/method-interval': {
-
-    }
-    case '/main/english-puzzle': {
-
-    }
-    case '/main/speak-it': {
-
-    }
-    case '/main/sprint': {
-
-    }
-    case '/main/listening': {
-
-    }
-    case '/main/savannah': {
-
-    }
-    case '/main/constructor': {
-
-    }
-    case '/vocabulary': {
-
-    }
-    case '/statistic': {
-
-    }
-    case '/about-us': {
-
+    default: {
+      return 'default';
     }
   }
 };
--- src/js/router/routerComponents.js
+++ src/js/router/routerComponents.js
@@ -8,51 +8,50 @@
 * pageGame() - название функции, которая отдает вашу начальную верстку для игры.
 */
 
-import { createErrorPage, createSignInUpPage } from "../pages"
+import { createErrorPage, createSignInUpPage } from '../pages';
 
 export const HomeComponent = {
   render: () => '<a href="#/auth">click</a>',
-}
+};
 export const AuthComponent = {
   render: () => createSignInUpPage(),
-}
+};
 export const MainComponent = {
   render: () => '',
-}
+};
 export const SettingsComponent = {
   render: () => '',
-}
+};
 export const LearningComponent = {
-  render: () => promo() + page(),
-}
+  render: () => '',
+};
 export const MethodIntervalComponent = {
   render: () => '',
-}
+};
 export const EnglishPuzzleComponent = {
   render: () => '',
-}
+};
 export const SpeakItComponent = {
   render: () => '',
-}
+};
 export const ListeningComponent = {
   render: () => '',
-}
+};
 export const SavannahComponent = {
   render: () => '',
-}
+};
 export const ConstructorComponent = {
   render: () => '',
-}
+};
 export const VocabularyComponent = {
   render: () => '',
-}
+};
 export const StatisticComponent = {
   render: () => '',
-}
+};
 export const AboutUsComponent = {
   render: () => '',
-}
-
+};
 export const ErrorComponent = {
   render: () => createErrorPage(),
-}
+};
