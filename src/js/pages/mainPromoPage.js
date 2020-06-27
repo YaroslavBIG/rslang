87f42711e9926def6263275fa717913f7a00e0bb
Yulia Kravtsova <KostyaEK2006@gmail.com>
2020-06-27 21:11:02 +0300

    feat: add beutiful indent

--- src/js/pages/mainPromoPage.js
+++ src/js/pages/mainPromoPage.js
@@ -8,44 +8,36 @@ export const mainPromoPage = () => `
   <div class="modal-body">
     <span class="modal-body__mini-games">Огромное разнообразие мини-игр</span>
     <div class="modal-body__first-gifs">
-    <img src="./assets/site-functional.gif" class="modal-body__gifs-block">
-    <img src="./assets/site-functional1.gif" class="modal-body__gifs-block">
-    <img src="./assets/site-functional2.gif" class="modal-body__gifs-block">
+      <img src="./assets/site-functional.gif" class="modal-body__gifs-block">
+      <img src="./assets/site-functional1.gif" class="modal-body__gifs-block">
+      <img src="./assets/site-functional2.gif" class="modal-body__gifs-block">
     </div>
     <div class="modal-body__second-gifs">
-    <img src="./assets/site-functional3.gif" class="modal-body__gifs-block">
-    <img src="./assets/site-functional4.gif" class="modal-body__gifs-block">
-    <img src="./assets/site-functional5.gif" class="modal-body__gifs-block">
+      <img src="./assets/site-functional3.gif" class="modal-body__gifs-block">
+      <img src="./assets/site-functional4.gif" class="modal-body__gifs-block">
+      <img src="./assets/site-functional5.gif" class="modal-body__gifs-block">
     </div>
   </div>
   <div class="modal-footer">
-  <span class="modal-footer__interface">Легкий в понимании интерфейс</span>
-  <!-- Slider main container -->
-  <div class="swiper-container">
-	  <!-- Additional required wrapper -->
-	  <div class="swiper-wrapper">
-		  <!-- Slides -->
-		  <div class="swiper-slide">Slide 1</div>
-		  <div class="swiper-slide">Slide 2</div>
-		  <div class="swiper-slide">Slide 3</div>
-		  ...
-	  </div>
-	  <!-- If we need pagination -->
-	  <div class="swiper-pagination"></div>
-  
-	  <!-- If we need navigation buttons -->
-	  <div class="swiper-button-prev"></div>
-	  <div class="swiper-button-next"></div>
-  
-	  <!-- If we need scrollbar -->
-	  <div class="swiper-scrollbar"></div>
-  </div>
-  <span class="modal-footer__familiarize">Краткий обзор приложения</span>
-  <iframe src="https://www.youtube.com/embed/f4ioMGDQblI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="modal-footer__video"></iframe>
-  <div class="modal-footer__go-block">
-  <span class="modal-footer__go">Вперёд! К новым начинаниям!</span>
-  </div>
-  <button class="modal-footer__button-start" ><a href="#/auth" class="modal-footer__button-name">Вперёд!</a></button>
+    <span class="modal-footer__interface">Легкий в понимании интерфейс</span>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+        ...
+      </div>
+      <div class="swiper-pagination"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-scrollbar"></div>
+    </div>
+    <span class="modal-footer__familiarize">Краткий обзор приложения</span>
+    <iframe src="https://www.youtube.com/embed/f4ioMGDQblI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="modal-footer__video"></iframe>
+    <div class="modal-footer__go-block">
+      <span class="modal-footer__go">Вперёд! К новым начинаниям!</span>
+    </div>
+    <button class="modal-footer__button-start" ><a href="#/auth" class="modal-footer__button-name">Вперёд!</a></button>
   </div>
 </div>
 </div>
