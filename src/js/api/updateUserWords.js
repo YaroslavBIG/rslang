ea18afe50024dca958617a29aa6522ca0e9c6ad3
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-06-27 17:03:25 +0300

    refactor: change logic of func update user word

--- src/js/api/updateUserWords.js
+++ src/js/api/updateUserWords.js
@@ -1,17 +1,10 @@
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZTg5MjI1MTJkYWJhMDAxN2JkYzk1NyIsImlhdCI6MTU5MzAyMjkwNSwiZXhwIjoxNTkzMDM3MzA1fQ.MnEp9DKVBV6mlkquu2zyD75s_VZ2xJf50C5oIz9NB6U';
+import { getResponse } from './getResponse';
+import { load } from '../utils/load';
+import { globalUser } from '../utils/main';
 
-export const updateUserWords = async (userId, wordId, wordOption) => {
-  const rawResponse = await fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userId}/words/${wordId}`, {
-    method: 'PUT',
-    withCredentials: true,
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(wordOption),
-  });
-  const content = await rawResponse.json();
-
-  return content;
+export const updateUserWords = async (wordId, wordOption) => {
+  load();
+  const userId = globalUser.get()[0].id || '5ee8922512daba0017bdc957';
+  const url = `users/${userId}/words/${wordId}`;
+  await getResponse(url, { method: 'PUT', body: JSON.stringify(wordOption) });
 };
