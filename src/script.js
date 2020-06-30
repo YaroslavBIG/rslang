caa580c9dd65e43db107033076ec126c48f063ff
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-06-30 17:34:29 +0300

    feat: delete uneccesary code

--- src/script.js
+++ src/script.js
@@ -1,7 +1,5 @@
 import { load } from './js/utils';
-import { intervalRepeatPromoPage } from './js/pages/intervalRepeatPromoPage';
 
 window.onload = () => {
   load();
-  document.body.innerHTML = intervalRepeatPromoPage();
 };
