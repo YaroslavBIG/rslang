0f515d434461ddba32b6483aa9f15ce16add7170
ElfimovArtem <elfimov021@yandex.ru>
2020-06-30 13:53:29 +0300

    feat: about the team page implemented.

---
+++ src/js/aboutTheTeamPageComponents/createMentorCard.js
@@ -0,0 +1,23 @@
+export const createMentorCard = () => `
+<div class="about-team__mentor-card">
+    <div class="mentor-card__view-block">
+        <div class="view-block__avatar mentor avatar-Ivan"></div>
+        <div class="view-block__social">
+          <a class="social-link" href="https://github.com/ikisialiou">
+            <i class="fa fa-github"></i>
+          </a>
+          <a class="social-link" href="mailto:ivan_kiselev1990@mail.ru">
+            <i class="fa fa-envelope"></i>
+          </a>
+        </div>
+    </div>
+    <div class="mentor-card__description-block center">
+        <h2 class="description-block__name">Киселев Иван</h2>
+        <p class="description-block__role">Ментор / Наставник</p>
+        <p class="description-block__text mentor-text">
+          « Мы постарались сделать уникальное, в своем роде, приложение, которое поможет каждому, кто хочет выучить 
+          английский язык. Заходите, регистрируйтесь и дерзайте! »
+        </p>
+    </div>
+</div>
+`;
---
+++ src/js/aboutTheTeamPageComponents/createTeamCards.js
@@ -0,0 +1,112 @@
+export const createTeamCards = () => `
+    <div class="about-team__team-cards">
+        <div class="team-cards__membership-card">
+            <div class="membership-card__view-block">
+                <div class="view-block__avatar student avatar-Yaroslav"></div>
+                <div class="view-block__social">
+                  <a class="social-link" href="https://github.com/YaroslavBIG">
+                    <i class="fa fa-github"></i>
+                  </a>
+                  <a class="social-link" href="mailto:yaroslavbig@ya.ru">
+                    <i class="fa fa-envelope"></i>
+                  </a>
+                </div>
+            </div>
+            <div class="membership-card__description-block left">
+                <h2 class="description-block__name">Мос Ярослав</h2>
+                <p class="description-block__text">« Одна из техник, которую мы использовали в нашем приложении - метод
+                 интервальных повторений. Его разработал американский лингвист Пол Пимслер в 1967 году.»</p>
+            </div>
+        </div>
+        <div class="team-cards__membership-card right-card">
+            <div class="membership-card__description-block right">
+                <h2 class="description-block__name">Ясновская Карина</h2>
+                <p class="description-block__text">« Наше приложение поможет любому человеку, который хочет выучить язык
+                 с нуля или повысить уровень своих знаний »</p>
+            </div>
+            <div class="membership-card__view-block">
+                <div class="view-block__avatar student avatar-Rina"></div>
+                <div class="view-block__social">
+                  <a class="social-link" href="https://github.com/RinaYasnovskaya">
+                    <i class="fa fa-github"></i>
+                  </a>
+                  <a class="social-link" href="mailto:rina.yasnovskaya@gmail.com">
+                    <i class="fa fa-envelope"></i>
+                  </a>
+                </div>
+            </div>
+        </div>
+        <div class="team-cards__membership-card">
+            <div class="membership-card__view-block">
+                <div class="view-block__avatar student avatar-Natalia"></div>
+                <div class="view-block__social">
+                  <a class="social-link" href="https://github.com/NataliaK96">
+                    <i class="fa fa-github"></i>
+                  </a>
+                  <a class="social-link" href="mailto:ng_komarova@mail.ru">
+                    <i class="fa fa-envelope"></i>
+                  </a>
+                </div>
+            </div>
+            <div class="membership-card__description-block left">
+                <h2 class="description-block__name">Комарова Наталья</h2>
+                <p class="description-block__text">« Обычно занятия носят лекционный характер с элементами общения. 
+                У нас же - весь процесс построен на играх, которые упрощают процесс понимания и изучения »</p>
+            </div>
+        </div>
+        <div class="team-cards__membership-card right-card">
+            <div class="membership-card__description-block right">
+                <h2 class="description-block__name">Константин Кравцов</h2>
+                <p class="description-block__text">« В нашем приложении есть полная статистика всего процесса обучения, 
+                 который проходит пользователь. Это значительно облегчит изучение языка. »</p>
+            </div>
+            <div class="membership-card__view-block">
+                <div class="view-block__avatar student avatar-Doge"></div>
+                <div class="view-block__social">
+                  <a class="social-link" href="https://github.com/Yeeeeee1">
+                    <i class="fa fa-github"></i>
+                  </a>
+                  <a class="social-link" href="mailto:Yeeeeee1@yandex.ru">
+                    <i class="fa fa-envelope"></i>
+                  </a>
+                </div>
+            </div>
+        </div>
+        <div class="team-cards__membership-card">
+            <div class="membership-card__view-block">
+                <div class="view-block__avatar student avatar-Vadim"></div>
+                <div class="view-block__social">
+                  <a class="social-link" href="https://github.com/VadimRomanovski">
+                    <i class="fa fa-github"></i>
+                  </a>
+                  <a class="social-link" href="mailto:mr.vromanovski@gmail.com">
+                    <i class="fa fa-envelope"></i>
+                  </a>
+                </div>
+            </div>
+            <div class="membership-card__description-block left">
+                <h2 class="description-block__name">Романовский Вадим</h2>
+                <p class="description-block__text">« Все на столько просто и понятно, что стартовый уровень Ваших знаний 
+                не имеет значения! Главное начать обучение! »</p>
+            </div>
+        </div>
+        <div class="team-cards__membership-card right-card">
+            <div class="membership-card__description-block right">
+                <h2 class="description-block__name">Елфимов Артем</h2>
+                <p class="description-block__text">« Наше приложение позволит Вам учиться из любой точки мира и с любого 
+                 устройства. От Вас потребуется только желание и подключение к сети Интернет. »</p>
+            </div>
+            <div class="membership-card__view-block">
+                <div class="view-block__avatar student avatar-Elfimov"></div>
+                <div class="view-block__social">
+                  <a class="social-link" href="https://github.com/ElfimovArtem">
+                    <i class="fa fa-github"></i>
+                  </a>
+                  <a class="social-link" href="mailto:elfimov021@yandex.ru">
+                    <i class="fa fa-envelope"></i>
+                  </a>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
