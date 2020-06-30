56ffd5b84e2925fe186089e70dfa6faf83eedc00
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-06-30 11:47:47 +0300

    feat: add first version

---
+++ src/js/pages/intervalRepeatPromoPage.js
@@ -0,0 +1,23 @@
+export const intervalRepeatPromoPage = () => `
+<div id="myModal" class="modal">
+<div class="modal-content">
+  <div class="modal-header">
+  <a href="#/" class="close"></a>
+    <span class="modal-header__name">Метод интервального повторения</span> 
+  </div>
+  <div class="modal-body">
+    <h3>Система Лейтнера и карточки для повторений</h3>
+    <p class="modal-body__description">Метод Лейтнера является одним из ярких примеров применения интервальных повторений с помощью бумажных карточек. Его используют при изучении иностранных слов, которые повторяют в разные промежутки, в зависимости от результата воспроизведения по памяти.</p>
+    <h3>Описание метода</h3>
+    <p class="modal_body__description">Здесь будет описание метода...</p>
+    <h3>Виды интервалов</h3>
+    <ul class="modal-body__list">
+      <li>Снова</li>
+    </ul>
+    </div>
+  <div class="modal-footer">
+    <button class="modal-footer__button-start" id="constructorGame"><span class="modal-footer__button-name">Продолжить</span></button>
+  </div>
+</div>
+</div>
+`;
--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import { load } from './js/utils';
+import { intervalRepeatPromoPage } from './js/pages/intervalRepeatPromoPage';
 
 window.onload = () => {
   load();
+  document.body.innerHTML = intervalRepeatPromoPage();
 };
