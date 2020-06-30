c14644edd5751816c0b3ee79b1b74d3187de7d88
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-06-30 13:24:45 +0300

    feat: add export promo page in index

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,3 +1,4 @@
 export { createErrorPage } from './createErrorPage';
 export { createSignInUpPage } from './createSignInUpPage';
 export { createMainPage } from './createMainPage';
+export { inttervalRepeatPromoPage } from './intervalRepeatPromoPage';
