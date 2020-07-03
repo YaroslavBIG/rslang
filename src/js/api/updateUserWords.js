acaccd29898ad0cb7eff02e038e49a96f7cc12f3
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-03 15:00:35 +0300

    refactor: refactor updateUserWords

--- src/js/api/updateUserWords.js
+++ src/js/api/updateUserWords.js
@@ -1,5 +1,4 @@
 import { getResponse } from './getResponse';
-import { load } from '../utils/load';
 import { globalUser } from '../utils/main';
 
 /**
@@ -21,7 +20,6 @@ import { globalUser } from '../utils/main';
  */
 
 export const updateUserWords = async (wordId, wordOption) => {
-  load();
   const userId = globalUser.get()[0].id || '5ee8922512daba0017bdc957';
   const url = `users/${userId}/words/${wordId}`;
   await getResponse(url, { method: 'PUT', body: JSON.stringify(wordOption) });
