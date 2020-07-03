9f517ac99683c4bcd17a4fb370ffcae82759c0cd
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-03 14:28:57 +0300

    refactor: change description of func

--- src/js/api/getUserWords.js
+++ src/js/api/getUserWords.js
@@ -3,21 +3,21 @@ import { load } from '../utils/load';
 import { globalUser } from '../utils/main';
 
 /**
- * Update user words
- * Функция для изменения свойств объекта слова по его id в словах пользователя
- * @param {string} wordId id слова, которое нужно изменить
- * @param {object} wordOption объект с стандартными свойствами объекта слова 
- * @example wordOptionn = {
+ * Get user words
+ * метод для получения слов пользователя
+ * @param {string} userId id пользователя
+ * @return {Array} возвращвет массив объектов слов пользователя
+ * @example [{
     "id": "5ee9241e12daba0017bdd5a8",
     "difficulty": string, weak - легкое, hard - тяжёлое
     "optional": {
-      "isDelete": boolean, если true помещает слово в удаленные
+      "isDelete": boolean, помещает слово в удаленные
       "repeatNumber": number, количество повторений
       "lastRepeat": number, последний раз повторялось
       "nextRepeat": number следующее повторение
     },
     "wordId": "5e9f5ee35eb9e72bc21af716"
-  }
+  }]
  */
 
 export const getUserWords = async () => {
