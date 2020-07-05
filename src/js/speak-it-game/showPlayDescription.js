1b06700f4d47f503ab89ca4237ff0ce071b271a4
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-07-05 22:52:27 +0400

    feat: add active style for item

--- src/js/speak-it-game/showPlayDescription.js
+++ src/js/speak-it-game/showPlayDescription.js
@@ -5,6 +5,8 @@ export const showPlayDescription = () => {
 
   document.querySelectorAll('.answers__item').forEach((item) => {
     item.addEventListener('click', (ev) => {
+      document.querySelectorAll('.answers__item').forEach((elem) => elem.classList.remove('item_active'));
+      item.classList.add('item_active');
       const bodyTranslate = (ev.path[0].children[3] === undefined) ? ev.path[1] : ev.path[0];
       document.querySelector('[alt="speak-blank"]').src = `${dataUrl}${item.dataset.img}`;
       document.querySelector('.hints-translation').textContent = bodyTranslate.children[3].textContent;
