b2402e138318ebc777c2862760541116b1ba2c10
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-07-07 20:46:16 +0400

    feat: add show input & disabled item when start game

---
+++ src/js/speak-it-game/clickStart.js
@@ -0,0 +1,10 @@
+export const clickStart = () => {
+  document.querySelector('#start-speak').addEventListener('click', () => {
+    document.querySelectorAll('.answers__item').forEach((el) => {
+      el.style = 'pointer-events: none';
+      el.classList.remove('item_active');
+    });
+    document.querySelector('.hints-translation').classList.add('hidden');
+    document.querySelector('.hints-input').classList.remove('hidden');
+  });
+};
--- src/js/speak-it-game/startSpeakGame.js
+++ src/js/speak-it-game/startSpeakGame.js
@@ -1,6 +1,7 @@
 import { getWords } from '../api';
 import { getAllCards } from './getAllCards';
 import { showPlayDescription } from './showPlayDescription';
+import { clickStart } from './clickStart';
 
 export const startSpeakGame = async (page, group, perPage) => {
   const result = await getWords(page, group, 10, perPage);
@@ -8,4 +9,5 @@ export const startSpeakGame = async (page, group, perPage) => {
   document.querySelector('.answers').innerHTML = resTemp;
 
   showPlayDescription();
+  clickStart();
 };
