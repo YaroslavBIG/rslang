c9a5febccb7e302ac8b9e7e0fc99b58d03d16cf3
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-07-07 19:57:41 +0400

    feat: add clear form when we got an error

--- src/js/api/addError.js
+++ src/js/api/addError.js
@@ -2,7 +2,7 @@ export const addError = () => {
   document.querySelector('.error-block-up').innerHTML = 'Возможно, такой пользователь уже существует';
   document.querySelector('.error-block-up').style = 'font-size: 13px';
   document.querySelector('.start-enter__inner').style = 'height: 520px';
-
+  document.getElementsByTagName('form')[1].reset();
   setTimeout(() => {
     document.querySelector('.error-block-up').innerHTML = '';
     document.querySelector('.start-enter__inner').style = 'height: 500px';
