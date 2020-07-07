1cf0603d3d063da729f9869e41bfddb96b3e0c87
ElfimovArtem <elfimov021@yandex.ru>
2020-07-07 15:54:57 +0300

    fix: background substrate added.

--- src/js/pages/createAboutTheTeamPage.js
+++ src/js/pages/createAboutTheTeamPage.js
@@ -5,5 +5,6 @@ export const createAboutTheTeamPage = () => `
     <h1 class="about-team__title">СТРАНА ДОЛЖНА ЗНАТЬ СВОИХ ГЕРОЕВ В ЛИЦО</h1>
     ${createMentorCard()}
     ${createTeamCards()}
+    <div class="about-team__substrate"></div>
 </section>
 `;
