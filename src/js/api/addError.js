66d0a4b38e76ad5e59d7490820b79f6129ece756
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-07-07 19:08:46 +0400

    feat: change some code

--- src/js/api/addError.js
+++ src/js/api/addError.js
@@ -2,4 +2,9 @@ export const addError = () => {
   document.querySelector('.error-block-up').innerHTML = 'Возможно, такой пользователь уже существует';
   document.querySelector('.error-block-up').style = 'font-size: 13px';
   document.querySelector('.start-enter__inner').style = 'height: 520px';
+
+  setTimeout(() => {
+    document.querySelector('.error-block-up').innerHTML = '';
+    document.querySelector('.start-enter__inner').style = 'height: 500px';
+  }, 2000);
 };
