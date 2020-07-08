f1c9b97ed17b010ddb565c561094d7b85b8cf27a
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-08 21:01:51 +0300

    feat: implement dictionary page

--- src/js/form/signUp/signUp.js
+++ src/js/form/signUp/signUp.js
@@ -1,6 +1,6 @@
 import { globalUser } from '../../utils';
 import { createUser } from '../../api';
-import { saveIcon } from './saveIcon';
+import { saveSettings } from './saveSettings';
 import { logIn } from '../signIn/login';
 
 export const signUp = async () => {
@@ -9,19 +9,21 @@ export const signUp = async () => {
 
   const login = document.querySelector('[name="email"]').value;
   const pass = document.querySelector('[name="password-up"]').value;
+  const nameUser = document.querySelector('[name="name"]').value;
   const reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[+-_@$!%*?&#.,;:[\]{}]).{8,}$/;
 
   if (reg.test(pass)) {
-    const user = { email: login, password: pass };
+    const user = { name: nameUser, email: login, password: pass };
 
+    globalUser.set('name', nameUser);
     globalUser.set('email', login);
     globalUser.set('password', pass);
 
     const createSuccess = await createUser(user);
     const logSuccess = await logIn(createSuccess);
-    const iconSuccess = await saveIcon(logSuccess);
+    const settingsSuccess = await saveSettings(logSuccess);
 
-    if (iconSuccess) {
+    if (settingsSuccess) {
       alert('Успешная регистрация!');
       nextButton.click();
     }
--- src/js/mini-game-sprint/repeatButtonHandler.js
+++ src/js/mini-game-sprint/repeatButtonHandler.js
@@ -4,8 +4,8 @@ import { createSprintGamePage } from '../pages/createSprintGamePage';
 export const repeatButtonHandler = () => {
   const repeatBtn = document.getElementById('repeat-btn');
   repeatBtn.onclick = () => {
-    const root = document.getElementById('root'); // Поставил для проверки не ругайте плиз!)))
-    root.innerHTML = createSprintGamePage(); // здесь надо вставить в иннер того блока, который будет основным при отрисовке
+    const root = document.querySelector('.main');
+    root.innerHTML = createSprintGamePage();
     startSprintGame();
   };
 };
