666f8cce1a13965e0fee978a835c964d9ddc3be9
YaroslavBIG <yaroslavbig@ya.ru>
2020-07-09 00:02:37 +0200

    refactor: update index.js

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -3,3 +3,7 @@ export { dataUrl } from './constants';
 export { createUser } from './createUser';
 export { getResponse } from './getResponse';
 export { loginUser } from './loginUser';
+export { getAggregatedWords } from './words/getAggregatedWords';
+export { getAllUserWords } from './words/getAllUserWords';
+export { getMixWords } from './words/getMixWords';
+export { userWord } from './words/userWord';
--- src/js/mainGame/utils/index.js
+++ src/js/mainGame/utils/index.js
@@ -1,3 +1,8 @@
 export { correctionBlock } from './correctionBlock';
 export { answerCheck } from './answerCheck';
 export { setStorageFromObject } from '../../utils/setStorageFromObject';
+export { cardCount } from './cardCount';
+export { getSettings } from './getSettings';
+export { isNewUserWord } from './isNewUserWord';
+export { playAudio } from './playAudio';
+export { setTranscription } from './setTranscription';
