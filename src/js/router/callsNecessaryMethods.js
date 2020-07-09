174a17218e476bee9885a292fcceb18e6b490a2e
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-09 23:28:53 +0300

    refactor: resolve merge problem

--- src/js/router/callsNecessaryMethods.js
+++ src/js/router/callsNecessaryMethods.js
@@ -1,23 +1,24 @@
 /**
-  * в switch каждый забирает свой кейс, чтобы не пушить пустые болванки.
-  * в нем пишет те функции, которые вызываются у него на странице.
-  * список кейсов:
-  * case '/main': {}
-  * case '/main/settings': {}
-  * case '/main/word-learning': {}
-  * case '/main/word-repeat': {}
-  * case '/main/english-puzzle': {}
-  * case '/main/speak-it': {}
-  * case '/main/audition': {}
-  * case '/main/savannah': {}
-  * case '/main/word-puzzle': {}
-  * case '/vocabulary': {}
-  * case '/statistic': {}
-  * case '/about-us': {}
-  */
+ * в switch каждый забирает свой кейс, чтобы не пушить пустые болванки.
+ * в нем пишет те функции, которые вызываются у него на странице.
+ * список кейсов:
+ * case '/main': {}
+ * case '/main/settings': {}
+ * case '/main/word-learning': {}
+ * case '/main/word-repeat': {}
+ * case '/main/english-puzzle': {}
+ * case '/main/speak-it': {}
+ * case '/main/audition': {}
+ * case '/main/savannah': {}
+ * case '/main/word-puzzle': {}
+ * case '/vocabulary': {}
+ * case '/statistic': {}
+ * case '/about-us': {}
+ */
 
 import { controlForm, submitForm, chooseIcon } from '../form';
 import { startSprintGame } from '../mini-game-sprint/launchSprintGameMethods';
+import { handlerSettingsPage, handlerMainPage } from '../pages';
 
 export const callsNecessaryMethods = (path) => {
   switch (path) {
@@ -30,8 +31,16 @@ export const callsNecessaryMethods = (path) => {
       chooseIcon();
       break;
     }
+    case '/main': {
+      handlerMainPage();
+      break;
+    }
     case '/main/sprint': {
       startSprintGame();
+      break;
+    }
+    case '/main/settings': {
+      handlerSettingsPage();
       break;
     }
     default: {
