643cc066d1ad7ca2896d50adbf3a3f350977872a
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-09 23:01:21 +0300

    refactor: refactor

--- src/js/api/getUserSettings.js
+++ src/js/api/getUserSettings.js
@@ -1,13 +1,10 @@
+import { loadUser } from '../utils';
 import { getResponse } from './getResponse';
-import { globalUser } from '../utils/main';
 
-/**
- * Get user settings
- * метод возвращает объект с настройками пользователя
- */
-
-export const getUserSettings = () => {
-  const { id: userId } = globalUser.get();
-  const url = `users/${userId}/settings`;
-  return getResponse(url, { method: 'GET' });
+export const getUserSettings = async () => {
+  const user = loadUser();
+  const resp = await getResponse(`users/${user.userId}/settings`, {
+    method: 'GET',
+  });
+  return resp;
 };
