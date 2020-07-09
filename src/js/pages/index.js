834740a1a3e1a3dce1199944df2d9750c4699f6b
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-09 23:30:52 +0300

    refactor: resolve merge problem

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,1 +1,8 @@
-export { router } from './router';
+export { createErrorPage } from './createErrorPage';
+export { createSignInUpPage } from './createSignInUpPage';
+export { createMainPage } from './createMainPage';
+export { createSettingsPage } from './createSettingsPage';
+export { handlerSettingsPage } from './handlerSettingsPage';
+export { createAboutTheTeamPage } from './createAboutTheTeamPage';
+export { createSprintGamePage } from './createSprintGamePage';
+export { handlerMainPage } from './handlerMainPage';
