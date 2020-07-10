eb7b37ecc197d501d879131477469db0750cc4c7
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-10 21:05:07 +0300

    Delete filterDictionaryWords.js

--- src/js/dictionary-page/filterDictionaryWords.js
+++
@@ -1,9 +0,0 @@
-export const filterDictionaryWords = (words, category) => {
-  if (category === 'all') {
-    return words.filter((word) => word.difficulty !== 'hard' && word.optional.deleted === false);
-  } if (category === 'hard') {
-    return words.filter((word) => word.difficulty === 'hard' && word.optional.deleted === false);
-  } if (category === 'delete') {
-    return words.filter((word) => word.optional.deleted === true);
-  }
-};
