8f45f0a1a701ce0793bca7451d29a815cd77d016
FireGun <50775604+YaroslavBIG@users.noreply.github.com>
2020-07-12 16:57:47 +0300

    fix: enter key

--- src/js/mainGame/buttons/enter.js
+++ src/js/mainGame/buttons/enter.js
@@ -1,9 +1,12 @@
 import { gameArrows } from './arrow';
 
 export const pressEnterKey = (event) => {
-  if (event.key === 'Enter') {
+  const modalWindow = document.querySelector('.game_main__modal');
+  const modalIsClose = modalWindow.style.display === 'unset';
+  console.log('тыц');
+  if (event.key === 'Enter' && modalIsClose) {
     event.preventDefault();
-
+    console.log('!!!!');
     gameArrows({ target: 'right' });
   }
 };
