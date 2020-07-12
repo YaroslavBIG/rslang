b18e7052e37b5957d58e91ca3827f90c36549d92
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-12 15:46:15 +0300

    refactor

--- src/js/dictionary-page/renderLayout.js
+++ src/js/dictionary-page/renderLayout.js
@@ -4,13 +4,6 @@ export const renderLayout = () => {
     <div class="dictinonary-buttons">
       <ul class="nav-dictionary__list">
           <li class="nav-dictionary__item">
-            <a href="#" class="nav-dictionary__link nav-dictionary__link_learning">
-              Изучаемые словаexport const renderLayout = () => {
-  const template = `
-    <div class="nav-dictionary">
-    <div class="dictinonary-buttons">
-      <ul class="nav-dictionary__list">
-          <li class="nav-dictionary__item">
             <button class="nav-dictionary__link nav-dictionary__link_learning">
               Изучаемые слова
             </button>
@@ -31,36 +24,6 @@ export const renderLayout = () => {
                 Главная
               </button>
             </a>
-          </li>
-      </ul>
-    </div>
-  </div>
-  <div class="dictonary-count">
-    Число слов:
-    <span class="dictonary-count__numder"></span>
-  </div>
-  <div class="dictonary-count dictonary-learned">
-  </div>
-  <div class="words-wrapper">
-  </div>
-    `;
-  return template;
-};
-
-            </a>
-          </li>
-          <li class="nav-dictionary__item">
-            <a href="#" class="nav-dictionary__link nav-dictionary__link_hard">
-              Сложные слова
-            </a>
-          </li>
-          <li class="nav-dictionary__item">
-            <a href="#" class="nav-dictionary__link nav-dictionary__link_deleted">
-              Удалённые слова
-            </a>
-          </li>
-          <li class="nav-dictionary__item">
-          <a href="#/main" class="nav-dictionary__link">Главная</a>
           </li>
       </ul>
     </div>
