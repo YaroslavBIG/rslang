d3a19c6f4c675fcf87a725e75aca082bc5e30100
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-12 15:41:48 +0300

    refactor: change nav func

--- src/js/dictionary-page/renderLayout.js
+++ src/js/dictionary-page/renderLayout.js
@@ -5,7 +5,48 @@ export const renderLayout = () => {
       <ul class="nav-dictionary__list">
           <li class="nav-dictionary__item">
             <a href="#" class="nav-dictionary__link nav-dictionary__link_learning">
+              Изучаемые словаexport const renderLayout = () => {
+  const template = `
+    <div class="nav-dictionary">
+    <div class="dictinonary-buttons">
+      <ul class="nav-dictionary__list">
+          <li class="nav-dictionary__item">
+            <button class="nav-dictionary__link nav-dictionary__link_learning">
               Изучаемые слова
+            </button>
+          </li>
+          <li class="nav-dictionary__item">
+            <button class="nav-dictionary__link nav-dictionary__link_hard">
+              Сложные слова
+            </button>
+          </li>
+          <li class="nav-dictionary__item">
+            <button class="nav-dictionary__link nav-dictionary__link_deleted">
+              Удалённые слова
+            </button>
+          </li>
+          <li class="nav-dictionary__item">
+            <a href="#/main" class="nav-dictionary__link nav-dictionary__link_main">
+              <button class="nav-dictionary__link">
+                Главная
+              </button>
+            </a>
+          </li>
+      </ul>
+    </div>
+  </div>
+  <div class="dictonary-count">
+    Число слов:
+    <span class="dictonary-count__numder"></span>
+  </div>
+  <div class="dictonary-count dictonary-learned">
+  </div>
+  <div class="words-wrapper">
+  </div>
+    `;
+  return template;
+};
+
             </a>
           </li>
           <li class="nav-dictionary__item">
