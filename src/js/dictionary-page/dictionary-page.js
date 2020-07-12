b2ec3082b71cca5223bf62a3285a24707f68bccb
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-12 13:20:53 +0300

    Delete dictionary-page.js

--- src/js/dictionary-page/dictionary-page.js
+++
@@ -1,9 +0,0 @@
-import { renderLayout } from './renderLayout';
-import { renderWordsData } from './renderWordsData';
-import { addEventListnersDictionary } from './addEventListnersDictionary';
-
-export const renderDictionaryPage = () => {
-  renderLayout();
-  // renderWordsData();
-  // addEventListnersDictionary();
-};
