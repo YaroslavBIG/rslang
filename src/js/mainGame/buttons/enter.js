fa4e75fd2db1a8e99161b7abb48c887b9cf81b9a
FireGun <50775604+YaroslavBIG@users.noreply.github.com>
2020-07-12 17:25:19 +0300

    fix: enter button modal

--- src/js/mainGame/buttons/enter.js
+++ src/js/mainGame/buttons/enter.js
@@ -1,12 +1,13 @@
 import { gameArrows } from './arrow';
 
 export const pressEnterKey = (event) => {
-  const modalWindow = document.querySelector('.game_main__modal');
-  const modalIsClose = modalWindow.style.display === 'unset';
-  console.log('тыц');
-  if (event.key === 'Enter' && modalIsClose) {
+  if (event.key === 'Enter' && document.querySelector('.game_main__modal').style.display !== 'unset') {
     event.preventDefault();
-    console.log('!!!!');
-    gameArrows({ target: 'right' });
+
+    const modalWindow = document.querySelector('.game_main__modal');
+    const modalIsClose = modalWindow.style.display !== 'unset';
+    if (modalIsClose) {
+      gameArrows({ target: 'right' });
+    }
   }
 };
