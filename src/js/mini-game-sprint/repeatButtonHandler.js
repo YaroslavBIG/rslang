1c9f19253c5b0e0287fbdfc014f9a93618788123
ElfimovArtem <elfimov021@yandex.ru>
2020-07-15 15:26:51 +0300

    fix: bug fixed.

--- src/js/mini-game-sprint/repeatButtonHandler.js
+++ src/js/mini-game-sprint/repeatButtonHandler.js
@@ -1,11 +1,14 @@
 import { startSprintGame } from './launchSprintGameMethods';
 import { createSprintGamePage } from '../pages';
+import { constantsData } from './constants';
 
 export const repeatButtonHandler = () => {
   const repeatBtn = document.getElementById('repeat-btn');
   if (repeatBtn) {
     repeatBtn.onclick = () => {
       const root = document.querySelector('.main');
+      constantsData.correctAnswersCounter = 0;
+      constantsData.wrongAnswersCounter = 0;
       root.innerHTML = createSprintGamePage();
       startSprintGame();
     };
