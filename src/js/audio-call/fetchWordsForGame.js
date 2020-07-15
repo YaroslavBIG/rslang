5aefd216907654c1ef51f216ee1e64a7cc720e76
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-15 16:01:56 +0300

    fix: fix request

--- src/js/audio-call/fetchWordsForGame.js
+++ src/js/audio-call/fetchWordsForGame.js
@@ -2,10 +2,14 @@ import { getWords } from '../api/getWords';
 import { getAggregatedWords } from '../api/words/getAggregatedWords';
 import { randomInteger } from '../utils/random';
 import { DATA } from './data';
+import { spinner } from '../spinner/spinner';
 
 export const fetchWordsForGame = async () => {
+  const rootContainer = document.querySelector('#root');
+  const loader = spinner();
+  rootContainer.prepend(loader);
   let wordsForGame = null;
-  const filter = { $and: [{ 'userWord.optional.deleted': false }] };
+  const filter = { $and: [{ 'userWord.optional.deleted': 'false' }] };
   const userWords = await getAggregatedWords(filter);
   const randomPage = randomInteger(0, 29);
   if (userWords[0].paginatedResults.length >= 15) {
