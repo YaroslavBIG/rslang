898c331e0739a475f85572648dca91026ffd0e26
ElfimovArtem <elfimov021@yandex.ru>
2020-07-15 16:45:30 +0300

    feat: Не правильно отрабатывает метод detWords... иногда проскакивает пустой массив на выходе

---
+++ src/js/savannah/getGameDataArr.js
@@ -0,0 +1,11 @@
+import { randomInteger } from '../utils';
+import { getWords } from '../api';
+
+export const getGameDataArr = async () => {
+  const randomPage = randomInteger(0, 19);
+  const randomLevel = randomInteger(0, 5);
+  const data = await getWords(randomPage, randomLevel);
+  console.log(data);
+  console.log(randomPage);
+  console.log(randomLevel);
+};
--- src/js/savannah/launchSavannahMethods.js
+++ src/js/savannah/launchSavannahMethods.js
@@ -2,9 +2,11 @@ import { voiceHintBtnHandler } from './voiceHintBtnHandler';
 import { createLivesContainer } from './createLivesContainer';
 import { randomInteger } from '../utils';
 import { fetchSavannahData } from './fetchSavannahData';
+import { getGameDataArr } from './getGameDataArr';
 
 export const launchSavannahMethods = async () => {
   voiceHintBtnHandler();
   createLivesContainer();
+  await getGameDataArr();
   await fetchSavannahData(randomInteger(0, 59), randomInteger(0, 5));
 };
