94995bc4ba63f20e7e3c4d5a3c72215f2b3d3a56
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-07-15 13:50:47 +0400

    fix: test swiper

--- src/js/swiper/swiper.js
+++ src/js/swiper/swiper.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper/bundle';
-import 'swiper/swiper-bundle.css';
+// import 'swiper/swiper-bundle.css';
 
 export function addSwiper() {
   const swiper = new Swiper('.swiper-container', {
